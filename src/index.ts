aae5cb12f94dc52b48d3a6eb937c9a55ba3847fb
Onur Adsay <onuradsay@gmail.com>
2025-12-29 10:50:19 +0300

    Refactor helper file names

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export { startRuntime } from './core/bootstrap';
 export * from './core/types';
-// Re-export feature modules individually for tree-shaking
 export * from './engines/maple';
 export * from './engines/maple/matcher';
+export { startRuntime } from './core/bootstrap';
 export { loadPrecalculatedProperties } from './engines/maple/property-extraction';
