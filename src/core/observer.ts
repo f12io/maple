ddb142757178bdc734f87b64fb0dba2ec30699e9
Onur Adsay <onuradsay@gmail.com>
2026-01-19 12:12:31 +0300

    Optimize mutation observer

--- src/core/observer.ts
+++ src/core/observer.ts
@@ -5,13 +5,27 @@ export function startObserver() {
 
   const observer = new MutationObserver((muts) => {
     for (const mut of muts) {
-      if (!(mut.target instanceof Element)) continue;
+      if (mut.type === 'childList') {
+        for (const node of mut.addedNodes) {
+          if (node instanceof Element) {
+            for (const srcClass of node.classList) {
+              generateStylesFromClass(srcClass);
+            }
+          }
+        }
+      }
 
-      for (const srcClass of mut.target.classList) {
-        generateStylesFromClass(srcClass);
+      if (mut.type === 'attributes' && mut.target instanceof Element) {
+        for (const srcClass of mut.target.classList) {
+          generateStylesFromClass(srcClass);
+        }
       }
     }
   });
+
+  for (const srcClass of document.documentElement.classList) {
+    generateStylesFromClass(srcClass);
+  }
 
   observer.observe(document.documentElement, {
     childList: true,
