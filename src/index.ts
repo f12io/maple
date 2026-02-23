14e6dc83e47b66a191b30de30b465696cf2cabb4
Celal Çağkan Uğuz <celal.cagkan@gmail.com>
2026-02-23 11:53:55 +0300

    Fix for maple extension usage

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+export { PRECALCULATED_PROP_TYPES } from 'internal:precalculated-prop-types';
+export { convert } from '../tests/helpers/convert.helper';
 export { startRuntime } from './core/bootstrap';
-export * from './core/types';
+export { buildRule } from './core/builder';
+export { PROP_UNIT_MAP, TRANSFORM_KEYS } from './core/constants/dictionaries';
+export { parseClass } from './core/parser-class';
+export * from './core/types.d';
+export { PRECALCULATED_PROP_ABBREVIATIONS } from './generated/precalculated-prop-abbreviations';
