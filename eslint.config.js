940cb2f9cba45bddd650d6484326220e632c41b5
Onur Adsay <onuradsay@gmail.com>
2026-06-20 16:24:52 +0300

    chore: ignore additional benchmark artifacts in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  globalIgnores(['dist', 'src/generated', 'examples/maple.js']),
+  globalIgnores(['dist', 'src/generated', 'examples/maple.js', 'examples/benchmarks/artifacts/maple.js']),
   {
     extends: [
       eslintJs.configs.recommended,
